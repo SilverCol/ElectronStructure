a66ca80805214e4d6384568a53a241f6bf323a1e
Mitja Vodnik <mitjavodnik@gmail.com>
2019-06-08 09:22:20 +0200

    will use spectral methods instead

--- Helium.cpp
+++ Helium.cpp
@@ -3,11 +3,13 @@
 //
 
 #include <iostream>
+#include <gsl/gsl_sf_laguerre.h>
 #include "Helium.h"
 
 
 namespace
 {
+    const size_t M = 100;
     const double epsilon = 1e-2;
     const double F = std::pow(3.141592653589793 / 6, 1.0/3.0);
     const double A = F * .0545 * std::pow(8.0 * 3.141592653589793 / 3.0, -1.0/3.0);
@@ -15,83 +17,39 @@ namespace
 }
 
 Helium::Helium(const std::vector<double>& u, double R) :
+m_basis(M),
 m_u(u),
+m_r(u.size()),
 m_V(u.size()),
 m_f(u.size()),
 m_h(R/u.size()),
-m_E(0.0)
+m_E(0.0),
+m_N(u.size())
 {
-    for (size_t r = 0; r < m_V.size(); ++r) m_V[r] = - 1/((r + 1)*m_h);
-    updateDensity();
-    // LDA_DFT();
+    m_H = gsl_matrix_calloc(M, M);
+    m_eigenVec = gsl_matrix_alloc(M, M);
+    m_eigenVal = gsl_vector_alloc(M);
+    m_eigenWorkspace = gsl_eigen_symmv_alloc(M);
+    // for (size_t r = 0; r < m_V.size(); ++r) m_V[r] = - 1/((r + 1)*m_h);
+    // updateDensity();
+    //LDA_DFT();
 }
 
-void Helium::shoot(double E)
+Helium::~Helium()
 {
-    std::transform(m_V.begin(), m_V.end(), m_f.begin(),
-            [E, this](double v) {return m_h*m_h*(E - v) / 6 + 1;}
-    );
-
-    m_u.front() = 1.0;
-    m_u[1] = m_u.front() * (1 + m_h * m_h * ((E - m_V[2])/12 + 7*(m_V[0] - E)/12 - m_h*m_h*(m_V[2] - E)*(m_V[0] - E)/9))
-                         / (1 + m_h*m_h*((E - m_V[1])/2 + 2*m_h*m_h*(m_V[1] - E)*(m_V[2] - E)/9));
-
-    for (size_t n = 2; n < m_u.size(); ++n)
-    {
-        m_u[n] = (m_u[n-1]*(12 - 10*m_f[n-1]) - m_u[n-2]*m_f[n-2])/m_f[n];
-    }
+    gsl_eigen_symmv_free(m_eigenWorkspace);
+    gsl_matrix_free(m_H);
+    gsl_matrix_free(m_eigenVec);
+    gsl_vector_free(m_eigenVal);
 }
 
-void Helium::updateDensity()
+void Helium::costructVarBasis()
 {
-    double E1 = -.55;
-    double u1 = 1.0;
-    double E2 = -.45;
-    double u2 = 1.0;
-
-    while(u1*u2 > 0)
+    for (size_t n = 1; n < M + 1; ++n)
     {
-        double delta = (E2 - E1) / 2;
-
-        E1 -= delta;
-        shoot(E1);
-        u1 = m_u.back();
-        if (std::abs(u1/m_u.front()) < epsilon)
+        for (size_t r = 0; r < m_N; ++r)
         {
-            m_E = E1;
-            m_u /= norm();
-            return;
-        }
-
-        E2 += delta;
-        shoot(E2);
-        u2 = m_u.back();
-        if (std::abs(u2/m_u.front()) < epsilon)
-        {
-            m_E = E2;
-            m_u /= norm();
-            return;
-        }
-    }
-
-    while(true)
-    {
-        double En = (E1 + E2)/2;
-        shoot(En);
-        if (std::abs(m_u.back()/m_u.front()) < epsilon)
-        {
-            m_E = En;
-            m_u /= norm();
-            return;
-        }
-        else if (u1*m_u.back() > 0)
-        {
-            u1 = m_u.back();
-            E1 = En;
-        }
-        else{
-            E2 = En;
-            u2 = m_u.back();
+            m_basis[n][r] = std::sqrt(4/std::pow(n, 5)) * std::exp(-m_r[r] / n) * gsl_sf_laguerre_n(n, 1.0, 2*m_r[r]/n);
         }
     }
 }
--- Helium.h
+++ Helium.h
@@ -9,6 +9,10 @@
 #include <cmath>
 #include <algorithm>
 #include <numeric>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_eigen.h>
 
 inline void operator/=(std::vector<double>& v, double a)
 {
@@ -30,20 +34,31 @@ class Helium
 {
 public:
     Helium(const std::vector<double>& u, double R);
+    ~Helium();
     inline double energy() const {return m_E;}
     inline double norm() const {return ::norm(m_u, m_h);}
     std::vector<double> electrostatic();
 
 private:
-    void shoot(double E);
+    inline void constructSpace(){ for (size_t n = 0; n < m_N; ++n) m_r[n] = (n + 1) * m_h; }
+    void costructVarBasis();
     void updateDensity();
     void LDA_DFT();
 
+    gsl_matrix* m_H;
+    gsl_matrix* m_eigenVec;
+    gsl_vector* m_eigenVal;
+    gsl_eigen_symmv_workspace* m_eigenWorkspace;
+
+    std::vector<std::vector<double> > m_basis;
     std::vector<double> m_u;
+    std::vector<double> m_r;
     std::vector<double> m_V;
     std::vector<double> m_f;
+
     double m_h;
     double m_E;
+    size_t m_N;
 
 };
 
